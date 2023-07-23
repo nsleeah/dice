6677f049fb3bd3b22adee5ac0cf571b8cab9cc5b
Leahnicoles <lnsimonn@hotmail.com>
2023-07-23 14:27:05 +0100

    feat: change dice image to dice six using img literal

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var diceImageView1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView1.image = UIImage(named: "DiceSix")
     }
 
 
